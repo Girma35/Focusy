ec14efb2ba6728dbf7eee5c06fbb5fdc5652852b
girma35 <girmawakeyo4@gmail.com>
2026-05-03 13:30:57 +0300

    feat: implement MethodChannel to toggle AppBlockerService state from Flutter

--- android/app/src/main/kotlin/com/focusy/focusy/MainActivity.kt
+++ android/app/src/main/kotlin/com/focusy/focusy/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.focusy.focusy
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.focusy/blocker"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            when (call.method) {
+                "startBlocking" -> {
+                    AppBlockerService.isBlockingEnabled = true
+                    result.success(null)
+                }
+                "stopBlocking" -> {
+                    AppBlockerService.isBlockingEnabled = false
+                    result.success(null)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
